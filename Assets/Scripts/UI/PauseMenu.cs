da95e88a867c714902dbba79e1b38c755d4f0095
Musab Fiqi <50223081+mfiqi@users.noreply.github.com>
2024-02-01 15:52:06 -0500

    Added Pause Menu Functionality

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,25 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 public class PauseMenu : MonoBehaviour
 {
-    bool isPaused;
+    private bool isPaused;
+    public GameObject pauseMenuUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        isPaused = false;
+        isPaused = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
+        {
             isPaused = !isPaused;
 
-        if (isPaused)
-            Time.timeScale = 0.0f;
-        else
-            Time.timeScale = 1.0f;
+            if (isPaused)
+                Pause();
+            else 
+                Resume();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
