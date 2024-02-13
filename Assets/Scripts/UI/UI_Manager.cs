b849919803b6b370b5361198206ca4299ad6fadf
Natphi7 <83423156+Natphi7@users.noreply.github.com>
2024-02-13 09:08:14 -0500

    ui changes

--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -9,12 +9,16 @@ public class UI_Manager : MonoBehaviour
 {
     public TMP_Text HP_Text;
     public TMP_Text Feathers_Text;
+    public Slider health;
+    public Slider feathers;
 
     // Update is called once per frame
     void Update()
     {
         float HPtoDisplay = GameObject.FindWithTag("Player").GetComponent<Swan>().healthPoints;
         int FeathersToDisplay = Swan.enemiesKilled;
+        health.value = HPtoDisplay;
+        feathers.value = FeathersToDisplay;
 
         HP_Text.text = HPtoDisplay.ToString();
         Feathers_Text.text = FeathersToDisplay.ToString();
