7184c451b975d8d94401a430c757c7f70b3b3d5a
Iconoclass <65254940+Iconoclass@users.noreply.github.com>
2024-02-25 19:37:33 -0500

    end level 1 cutscene refined

--- Assets/Scripts/GameLogic/CheckPointManager.cs
+++ Assets/Scripts/GameLogic/CheckPointManager.cs
@@ -9,12 +9,11 @@ public class CheckPointManager : MonoBehaviour
     // The beginning of the level
     public static Vector3 lastCheckPointPos = new Vector3(-9.5f, 1, 0.5f);
     
-    //Next to the train station
+    // the train station
     //public static Vector3 lastCheckPointPos = new Vector3(140, 10, 1);
 
     private void Awake()
     {
         GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPos;
     }
-
 }
