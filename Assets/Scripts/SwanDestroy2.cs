c01822591a2514cb96b1aba58c3f36c78e49abaf
Paulyg6363 <140662296+Paulyg6363@users.noreply.github.com>
2024-02-27 23:34:21 -0500

    script change

--- Assets/Scripts/SwanDestroy2.cs
+++ Assets/Scripts/SwanDestroy2.cs
@@ -8,7 +8,7 @@ public class SwanDestroy2 : MonoBehaviour
     public Collider playerCollider;
     public AudioSource destroySoundSource;
     public Image screenFadeImage;
-    public TriggerCredits triggerCredits;
+    
 
     private bool isFading = false;
 
@@ -27,19 +27,6 @@ public class SwanDestroy2 : MonoBehaviour
         // Play the destroy sound
         destroySoundSource.Play();
 
-        // Enable the screen fade image
-        screenFadeImage.enabled = true;
-
-        // Wait for 10 seconds before triggering credits
-        StartCoroutine(WaitAndTriggerCredits());
-    }
-
-    IEnumerator WaitAndTriggerCredits()
-    {
-        yield return new WaitForSeconds(10f);
-
-        // Trigger the credits by enabling the associated GameObject
-        triggerCredits.gameObject.SetActive(true);
     }
 }
 
